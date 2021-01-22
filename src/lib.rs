557774b0a2538405115c2eb6e1506011f15fc655
Caleb Boylan <calebboylan@gmail.com>
2021-01-21 23:25:52 -0800

    Dont require boxed futures!

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,10 @@ use std::sync::Arc;
 use std::time::{Duration, Instant};
 use tokio::sync::{Notify, RwLock, RwLockReadGuard};
 
-//type RFuncRet<T> = Future<Output = Result<(T, Instant, Instant), ()>>;
-
-struct DogpileCache<T> {
+struct DogpileCache<T, F: Future<Output = Result<(T, Instant, Instant), ()>>> {
     cache_data: Arc<RwLock<CacheData<T>>>,
     refreshed: Arc<Notify>,
-    refresh_fn: fn() -> Box<dyn Future<Output = Result<(T, Instant, Instant), ()>> + Unpin>,
+    refresh_fn: fn() -> F,
 }
 
 pub struct CacheData<T> {
@@ -16,7 +14,7 @@ pub struct CacheData<T> {
     expire_time: Instant,
 }
 
-impl<T> Clone for DogpileCache<T> {
+impl<T, F: Future<Output = Result<(T, Instant, Instant), ()>>> Clone for DogpileCache<T, F> {
     fn clone(&self) -> Self {
         Self {
             cache_data: self.cache_data.clone(),
@@ -26,11 +24,10 @@ impl<T> Clone for DogpileCache<T> {
     }
 }
 
-impl<T: Send + Sync + 'static> DogpileCache<T> {
-    pub async fn create(
-        init: T,
-        refresh_fn: fn() -> Box<dyn Future<Output = Result<(T, Instant, Instant), ()>> + Unpin>,
-    ) -> Self {
+impl<T: Send + Sync + 'static, F: Future<Output = Result<(T, Instant, Instant), ()>>>
+    DogpileCache<T, F>
+{
+    pub async fn create(init: T, refresh_fn: fn() -> F) -> Self {
         let cache_data = Arc::new(RwLock::new(CacheData {
             value: init,
             expire_time: Instant::now(),
@@ -65,19 +62,19 @@ mod test {
     use tokio::time::sleep;
     #[tokio::test]
     async fn test1() {
-        //let valid_length = Duration::from_millis(20);
         let sleep_length = Duration::from_millis(21);
-        fn num() -> Box<dyn Future<Output = Result<(i32, Instant, Instant), ()>> + Unpin> {
+        async fn num() -> Result<(i32, Instant, Instant), ()> {
             let valid_length = Duration::from_millis(20);
-            Box::new(std::future::ready(Ok((
+            Ok((
                 1,
                 Instant::now() + valid_length,
                 Instant::now() + valid_length,
-            ))))
+            ))
         }
-        let c = DogpileCache::<i32>::create(0, num).await;
+        let c = DogpileCache::<i32, _>::create(0, num).await;
         assert_eq!(c.read().await.value, 1);
         c.cache_data.write().await.value = 2;
+        assert_eq!(c.read().await.value, 2);
         assert_eq!(c.read().await.value, 2);
         sleep(sleep_length).await;
         assert_eq!(c.read().await.value, 1);
